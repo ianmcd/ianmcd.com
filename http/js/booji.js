2190bc551ce46f3adcf3cc6e89887fd9a7a54e34
Ian McDowell <ianmcd@gmail.com>
2015-09-30 22:31:51 -0500

    minor css tweaks, added default image and nav js adjustments

--- http/js/booji.js
+++ http/js/booji.js
@@ -16,14 +16,50 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
 (function () {
   'use strict';
 
+  function openMenu() {
+    var toggleWrapper = $('#js-toggle-wrapper').detach();
+    $('body').prepend(toggleWrapper);
+    $('.page-inner').css('padding-top', '70px');
+    $('#js-site-wrapper, #js-menu-toggle, #js-toggle-wrapper').addClass('js-menu-open');
+    if($('body').width() > 1100) {
+      $('#js-toggle-wrapper').css({
+        'max-width': '100%',
+        'width': '1100px'
+      }).animate({
+        'width': $('body').width()
+      }, 500, function() {
+        $(this).addClass('js-animated').css('width', '100%');
+      });
+    }
+  }
+
+  function closeMenu() {
+    var toggleWrapper = $('#js-toggle-wrapper').detach();
+    $('.page-inner').prepend(toggleWrapper);
+    if($('#js-toggle-wrapper').hasClass('js-animated')) {
+      var navWidth = $('body').width() > 1100 ? '1100px' : '100%';
+      console.log(navWidth);
+      $('#js-toggle-wrapper').animate({
+        'width': navWidth
+      }, 500, function() {
+        $(this).css({
+          'max-width': '',
+          'width': ''
+        }).removeClass('animated');
+      });
+    }
+    $('.page-inner').css('padding-top', '');
+    $('#js-site-wrapper, #js-menu-toggle, #js-toggle-wrapper').removeClass('js-menu-open');
+  }
+
   $(function() {
 
     // detects menu button click and open/closes when appropriate
     $('#js-menu-toggle').click(function(event) {
       if($('#js-site-wrapper').hasClass('js-menu-open')) {
-        $('#js-site-wrapper, #js-menu-toggle, #js-toggle-wrapper').removeClass('js-menu-open');
+        closeMenu();
       } else {
-        $('#js-site-wrapper, #js-menu-toggle, #js-toggle-wrapper').addClass('js-menu-open');
+        openMenu();
         event.stopPropagation();  // stops the detection occuring below as the menu is just now opening
       }
     });
@@ -38,7 +74,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
           (!menuTop.is(event.target) && menuTop.has(event.target).length === 0)) {
 
           // element not a child of either menu variable so we close it
-          $('#js-site-wrapper, #js-menu-toggle, #js-toggle-wrapper').removeClass('js-menu-open');
+          closeMenu();
         }
       }
     });
